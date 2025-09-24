74d4668c588e471e986033a38ac95a8073946b66
BraveRam <99811385+BraveRam@users.noreply.github.com>
2025-09-24 15:18:11 +0300

    Delete drizzle directory

--- drizzle/0000_smiling_calypso.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"telegram_id" integer,
-	"username" text,
-	"first_name" text,
-	"last_name" text,
-	"created_at" timestamp DEFAULT now()
-);
